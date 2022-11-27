83d9fa638ae11a65f9ead66f0317c42969b38c8b
NowickiBlazej <blazej.a.now@gmail.com>
2022-11-27 14:01:46 +0100

    Fixed encoding for sample input

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 # %%
+from parser import Parser
 from utils import *
-# %%
-fnf = FNF( [['a', 'b'], ['c', 'd']] )
-print(fnf)
-# %%
+
+path = "input2.txt"
+with open(path) as f:
+    input = f.read()
+
+parser = Parser(input)
+
+alphabet = parser.get_alphabet()
+productions = parser.get_productions()
+for p in productions:
+    print(p)
+
+relation = Relation(productions, alphabet)
+word = parser.get_word()
+print(relation.get_dependencies_str())
+print(relation.get_independencies_str())
+
+graph = Graph(relation, word)
+print(graph.FNF())
+print(graph)
--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Transaction:
+class Production:
     name: str
     inputs: List[str]
     outputs: List[str]
@@ -11,6 +11,14 @@ class Transaction:
     @property
     def all(self):
         return self.inputs + self.outputs
+
+    def __str__(self):
+        return (
+            f"({self.name}) "
+            + ", ".join(self.outputs)
+            + " <- "
+            + ", ".join(self.inputs)
+        )
 
 
 @dataclass
@@ -22,43 +30,75 @@ class FNF:
 
 
 class Relation:
-    def __init__(self, transactions: List[Transaction]) -> None:
-        self.transactions = transactions
-        self.D = []
-        self.I = []
+    def __init__(self, productions: List[Production], alphabet: List[str]) -> None:
+        self.transactions = productions
+        self.D = set()
+        self.I = set()
 
-        for x in transactions:
-            for y in transactions:
+        for x in productions:
+            for y in productions:
                 if any(symbol in y.all for symbol in x.outputs) or any(
-                    symbol in x.all for symbol in x.outputs
+                    symbol in x.all for symbol in y.outputs
                 ):
-                    self.D.append((x.name, y.name))
+                    self.D.add((x.name, y.name))
                 else:
-                    self.I.append((x.name, y.name))
+                    self.I.add((x.name, y.name))
 
-    def get_alphabet(self):
-        return list(map(lambda x: x.name, self.transactions))
+        for symbol in alphabet:
+            self.D.add((symbol, symbol))
 
-    def get_dependencies(self):
-        return self.D.copy()
+    def is_dependent(self, a: str, b: str):
+        return (a, b) in self.D
 
-    def get_independencies(self):
-        return self.I.copy()
+    def get_dependencies_str(self):
+        d = [f"({a},{b})" for a, b in sorted(list(self.D))]
+        return "D = {" + ",".join(d) + "}"
+
+    def get_independencies_str(self):
+        i = [f"({a},{b})" for a, b in sorted(list(self.I))]
+        return "I = {" + ",".join(i) + "}"
 
 
 class Graph:
     def __init__(self, relation: Relation, word: str) -> None:
-        alphabet = relation.get_alphabet()
-        self.g = [[False for _ in alphabet] for _ in alphabet]
-        n = len(alphabet)
+        self.word = word
+        self.relation = relation
+        self.g = [[False for _ in word] for _ in word]
+        n = len(word)
+        visited = {id: {id} for id in range(n)}
 
-        # zle
-        # for x, y in relation.get_dependencies():
-        #     self.g[x][y] = True
+        for j in range(1, n):
+            for i in range(j - 1, -1, -1):
+                if relation.is_dependent(word[i], word[j]):
+                    if not visited[i].issubset(visited[j]):
+                        self.g[i][j] = True
+                        visited[j].update(visited[i])
 
-        visited = {id: set(id) for id in range(n)}
+    def FNF(self):
+        n = len(self.word)
+        colors = [-1 for _ in range(n)]
+        for i in range(n):
+            self._color(colors, i, 0)
+        fnf = [[] for _ in range(max(colors) + 1)]
+        for i, symbol in enumerate(self.word):
+            fnf[colors[i]].append(symbol)
+        return FNF(fnf)
+
+    def _color(self, colors, i, color):
+        if colors[i] == -1:
+            colors[i] = color
+            for j in range(i + 1, len(self.word)):
+                if self.g[i][j]:
+                    self._color(colors, j, color + 1)
+
+    def __str__(self) -> str:
+        n = len(self.word)
+        edges = []
         for j in range(1, n):
             for i in range(j):
-                # jeśli jest krawędź to sprawdzamy czy ma być
-                # if visited[]
-                pass
+                if self.g[i][j]:
+                    edges.append(f"{i+1} -> {j+1}")
+
+        labels = [f"{i+1}[label={s}]" for i, s in enumerate(self.word)]
+
+        return "digraph g{\n" + "\n".join(edges + labels) + "\n}"
